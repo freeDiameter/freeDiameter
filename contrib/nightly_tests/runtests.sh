3ee1fe556ff4720ea7bf9ed711c2d932933483d9
Sebastien Decugis <sdecugis@gmail.com>
2010-06-29 15:56:05 +0900

    Some fixes in the test scripts

--- contrib/nightly_tests/runtests.sh
+++ contrib/nightly_tests/runtests.sh
@@ -24,6 +24,8 @@ fi
 rm -rf $WORKDIR
 mkdir $WORKDIR
 
+echo "Starting Nightly tests, time: "`date`
+
 # Retrieve the default parameters.
 wget "http://www.freediameter.net/hg/freeDiameter/raw-file/tip/CTestConfig.cmake" -O $WORKDIR/1_default.cmake
 if [ ! -e $WORKDIR/1_default.cmake ]; then
@@ -39,7 +41,7 @@ if [ ! -e $WORKDIR/2_tests.list ]; then
 fi
 
 # Now, for each test in the list
-for t in $(cat $WORKDIR/2_tests.list); do
+for t in $(cat $WORKDIR/2_tests.list | grep -v -e "^#"); do
    # Create the work environment
    mkdir $WORKDIR/$t
    
@@ -50,18 +52,20 @@ for t in $(cat $WORKDIR/2_tests.list); do
    
    # Path name, build configuration, ...
    cat >> $WORKDIR/$t/CTestScript.cmake << EOF
-##########################
-SET(CTEST_SOURCE_DIRECTORY "$WORKDIR/source")
-SET(CTEST_BINARY_DIRECTORY "$WORKDIR/$t/build")
+      ##########################
+      SET(CTEST_SOURCE_DIRECTORY "$WORKDIR/source")
+      SET(CTEST_BINARY_DIRECTORY "$WORKDIR/$t/build")
 
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_CONFIGURATION "Profiling")
-set(CTEST_BUILD_OPTIONS "")
+      set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+      set(CTEST_BUILD_CONFIGURATION "Profiling")
 
-set(WITH_MEMCHECK FALSE)
-set(WITH_COVERAGE TRUE)
-##########################
-EOF   
+      set(CTEST_BUILD_OPTIONS "")
+      set(CTEST_BUILD_NAME "Unamed test profile")
+
+      set(WITH_MEMCHECK FALSE)
+      set(WITH_COVERAGE TRUE)
+      ##########################
+EOF
    
    wget "http://www.freediameter.net/hg/freeDiameter/raw-file/tip/contrib/nightly_tests/$t.conf" -O $WORKDIR/$t/params.conf
    if [ ! -e $WORKDIR/$t/params.conf ]; then
@@ -74,45 +78,45 @@ EOF
    
    # Now, the remaining of the script
    cat >> $WORKDIR/$t/CTestScript.cmake << EOF
-#######################################################################
+       #######################################################################
 
-ctest_empty_binary_directory(\${CTEST_BINARY_DIRECTORY})
+       ctest_empty_binary_directory(\${CTEST_BINARY_DIRECTORY})
 
-find_program(CTEST_HG_COMMAND NAMES hg)
-find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
-find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+       find_program(CTEST_HG_COMMAND NAMES hg)
+       find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+       find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 
-# set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE \${CTEST_SOURCE_DIRECTORY}/tests/valgrind.supp)
+       # set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE \${CTEST_SOURCE_DIRECTORY}/tests/valgrind.supp)
 
-if(NOT EXISTS "\${CTEST_SOURCE_DIRECTORY}")
-  set(CTEST_CHECKOUT_COMMAND "\${CTEST_HG_COMMAND} clone http://www.freediameter.net/hg/freeDiameter \${CTEST_SOURCE_DIRECTORY}")
-endif()
+       if(NOT EXISTS "\${CTEST_SOURCE_DIRECTORY}")
+	 set(CTEST_CHECKOUT_COMMAND "\${CTEST_HG_COMMAND} clone http://www.freediameter.net/hg/freeDiameter \${CTEST_SOURCE_DIRECTORY}")
+       endif()
 
-set(CTEST_UPDATE_COMMAND "\${CTEST_HG_COMMAND}")
+       set(CTEST_UPDATE_COMMAND "\${CTEST_HG_COMMAND}")
 
-set(CTEST_CONFIGURE_COMMAND "\${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE:STRING=\${CTEST_BUILD_CONFIGURATION}")
-set(CTEST_CONFIGURE_COMMAND "\${CTEST_CONFIGURE_COMMAND} -DWITH_TESTING:BOOL=ON \${CTEST_BUILD_OPTIONS}")
-set(CTEST_CONFIGURE_COMMAND "\${CTEST_CONFIGURE_COMMAND} \\"-G\${CTEST_CMAKE_GENERATOR}\\"")
-set(CTEST_CONFIGURE_COMMAND "\${CTEST_CONFIGURE_COMMAND} \\"\${CTEST_SOURCE_DIRECTORY}\\"")
+       set(CTEST_CONFIGURE_COMMAND "\${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE:STRING=\${CTEST_BUILD_CONFIGURATION}")
+       set(CTEST_CONFIGURE_COMMAND "\${CTEST_CONFIGURE_COMMAND} -DWITH_TESTING:BOOL=ON \${CTEST_BUILD_OPTIONS}")
+       set(CTEST_CONFIGURE_COMMAND "\${CTEST_CONFIGURE_COMMAND} \\"-G\${CTEST_CMAKE_GENERATOR}\\"")
+       set(CTEST_CONFIGURE_COMMAND "\${CTEST_CONFIGURE_COMMAND} \\"\${CTEST_SOURCE_DIRECTORY}\\"")
 
-ctest_start("Nightly")
-ctest_update()
-ctest_configure()
-ctest_build()
-ctest_test()
-if (WITH_MEMCHECK AND CTEST_COVERAGE_COMMAND)
-  ctest_coverage()
-endif (WITH_MEMCHECK AND CTEST_COVERAGE_COMMAND)
-if (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
-  ctest_memcheck()
-endif (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
-# ctest_submit()
+       ctest_start("Nightly")
+       ctest_update()
+       ctest_configure()
+       ctest_build()
+       ctest_test()
+       if (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
+	 ctest_coverage()
+       endif (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
+       if (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
+	 ctest_memcheck()
+       endif (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
+       ctest_submit()
 EOF
 
-   # Test until here for now...
-   echo "Ready to start $WORKDIR/$t/CTestScript.cmake, abort for now"
-   exit 1;
+   # OK, now run this test, it will submit its results.
+   ctest -S $WORKDIR/$t/CTestScript.cmake -V
 done
 
+echo "Completed Nightly tests, time: "`date`
 
 
