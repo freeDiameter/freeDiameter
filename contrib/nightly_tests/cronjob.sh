dfaf950b187e2dfc8ba13683343a26af9ebf3efc
Sebastien Decugis <sdecugis@gmail.com>
2010-07-08 16:49:08 +0900

    Fix script name -- all locations

--- contrib/nightly_tests/cronjob.sh
+++ contrib/nightly_tests/cronjob.sh
@@ -2,8 +2,8 @@
 
 # This script will simply retrieve the latest "runtest" script and run it.
 pushd ~/fDtests
-mv -f runtest.sh runtest.sh.prev
+mv -f runtests.sh runtests.sh.prev
 wget "http://www.freediameter.net/hg/freeDiameter/raw-file/tip/contrib/nightly_tests/runtests.sh"
-chmod +x runtest.sh
+chmod +x runtests.sh
 popd
-~/fDtests/runtest.sh
+~/fDtests/runtests.sh
