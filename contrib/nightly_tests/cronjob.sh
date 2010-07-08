41954e1bf905b29a97a130ed1ce495e0d6395df1
Sebastien Decugis <sdecugis@gmail.com>
2010-07-08 16:47:40 +0900

    Align forlder name on other script

--- contrib/nightly_tests/cronjob.sh
+++ contrib/nightly_tests/cronjob.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 # This script will simply retrieve the latest "runtest" script and run it.
-pushd ~/fDbuild
+pushd ~/fDtests
 mv -f runtest.sh runtest.sh.prev
 wget "http://www.freediameter.net/hg/freeDiameter/raw-file/tip/contrib/nightly_tests/runtest.sh"
 chmod +x runtest.sh
 popd
-~/fDbuild/runtest.sh
+~/fDtests/runtest.sh
