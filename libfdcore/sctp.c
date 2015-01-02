29578e35634fcab92e5009613e34863d21484075
Sebastien Decugis <sdecugis@gmail.com>
2015-01-02 23:24:26 +0800

    Fix compilation error as reported by Nikola in http://lists.freediameter.net/pipermail/help/2014-September/000860.html

--- libfdcore/sctp.c
+++ libfdcore/sctp.c
@@ -575,7 +575,7 @@ static int fd_setsockopt_prebind(int sk)
 		
 		struct sctp_event event;
 		
-		for (i = 0; i < (sizeof(events_I_want) / sizeof(events_I_want[0]) - 1; i++) {
+		for (i = 0; i < (sizeof(events_I_want) / sizeof(events_I_want[0]) - 1); i++) {
 			memset(&event, 0, sizeof(event));
 			event.se_type = events_I_want[i];
 			event.se_on = 1;
