4fac0c1415da63be98242720983752aaa6657424
nickvsnetworking <nick@nickvsnetworking.com>
2023-03-12 07:35:49 +1100

    Working with data from config

--- extensions/dbg_peers/dbg_peers.c
+++ extensions/dbg_peers/dbg_peers.c
@@ -5,44 +5,15 @@
 #include <freeDiameter/extension.h>
 #include <signal.h>
 
-#ifndef MONITOR_SIGNAL
-#define MONITOR_SIGNAL	SIGUSR2
-#endif /* MONITOR_SIGNAL */
+static int 	 monitor_peers_main(char * statefile);
 
-static int 	 monitor_main(char * conffile);
-
-EXTENSION_ENTRY("dbg_peers", monitor_main);
-
-
-
-/* Display information about a queue */
-static void display_info(char * queue_desc, char * peer, int current_count, int limit_count, int highest_count, long long total_count,
-			struct timespec * total, struct timespec * blocking, struct timespec * last)
-{
-	long long us = (total->tv_sec * 1000000) + (total->tv_nsec / 1000);
-	long double throughput = (long double)total_count * 1000000;
-	throughput /= us;
-	if (peer) {
-		TRACE_DEBUG(INFO, "'%s'@'%s': cur:%d/%d, h:%d, T:%lld in %ld.%06lds (%.2LFitems/s), blocked:%ld.%06lds, last processing:%ld.%06lds",
-			queue_desc, peer, current_count, limit_count, highest_count,
-			total_count, total->tv_sec, total->tv_nsec/1000, throughput,
-			blocking->tv_sec, blocking->tv_nsec/1000, last->tv_sec, last->tv_nsec/1000);
-	} else {
-		TRACE_DEBUG(INFO, "Global '%s': cur:%d/%d, h:%d, T:%lld in %ld.%06lds (%.2LFitems/s), blocked:%ld.%06lds, last processing:%ld.%06lds",
-			queue_desc, current_count, limit_count, highest_count,
-			total_count, total->tv_sec, total->tv_nsec/1000, throughput,
-			blocking->tv_sec, blocking->tv_nsec/1000, last->tv_sec, last->tv_nsec/1000);
-	}
-}
+EXTENSION_ENTRY("dbg_peers", monitor_peers_main);
 
 /* Thread to display periodical debug information */
 static pthread_t thr;
-static void * mn_thr(void * arg)
+static void * mn_thr(char * statefile)
 {
-#ifdef DEBUG
-	int i = 0;
-#endif
-	fd_log_threadname("Monitor thread");
+	fd_log_threadname("Monitor Diameter Peer thread");
 	char * buf = NULL;
 	size_t len;
 	
@@ -53,22 +24,14 @@ static void * mn_thr(void * arg)
 		struct timespec total, blocking, last;
 		struct fd_list * li;
 	
-		#ifdef DEBUG
-		for (i++; i % 30; i++) {
-			fd_log_debug("[dbg_peers] %ih%*im%*is", i/3600, 2, (i/60) % 60 , 2, i%60); /* This makes it easier to detect inactivity periods in the log file */
-			sleep(1);
-		}
-		#else /* DEBUG */
-		sleep(3599); /* 1 hour */
-		#endif /* DEBUG */
-		TRACE_DEBUG(INFO, "[dbg_peers] Dumping peer statistics to file");
+		TRACE_DEBUG(INFO, "[dbg_peers] Dumping peer statistics to file %s", statefile);
 		
 		/* Log to file */
 		FILE * fptr;
-		fptr = fopen("/tmp/DiamPeers.txt","w");
+		fptr = fopen(statefile,"w");
 		if(fptr == NULL)
 		{
-			TRACE_DEBUG(INFO, "[dbg_peers] Error loading file to write to");
+			TRACE_DEBUG(INFO, "[dbg_peers] Error loading file to write to at %s", statefile);
 			exit(1);             
 		}
 		fprintf(fptr, "%s", "Start of File\n");
@@ -78,14 +41,8 @@ static void * mn_thr(void * arg)
 		for (li = fd_g_peers.next; li != &fd_g_peers; li = li->next) {
 			struct peer_hdr * p = (struct peer_hdr *)li->o;
 			
-			TRACE_DEBUG(INFO, "%s", fd_peer_dump(&buf, &len, NULL, p, 1));
+			TRACE_DEBUG(INFO, "[dbg_peers] %s", fd_peer_dump(&buf, &len, NULL, p, 1));
 			fprintf(fptr, "%s %s", fd_peer_dump(&buf, &len, NULL, p, 1), "\n");
-			CHECK_FCT_DO( fd_stat_getstats(STAT_P_PSM, p, &current_count, &limit_count, &highest_count, &total_count, &total, &blocking, &last), );
-			display_info("Events, incl. recept", p->info.pi_diamid, current_count, limit_count, highest_count, total_count, &total, &blocking, &last);
-			
-			CHECK_FCT_DO( fd_stat_getstats(STAT_P_TOSEND, p, &current_count, &limit_count, &highest_count, &total_count, &total, &blocking, &last), );
-			display_info("Outgoing", p->info.pi_diamid, current_count, limit_count, highest_count, total_count, &total, &blocking, &last);
-			
 		}
 
 		fprintf(fptr, "%s", "End of File\n");
@@ -93,44 +50,35 @@ static void * mn_thr(void * arg)
 
 		CHECK_FCT_DO( pthread_rwlock_unlock(&fd_g_peers_rw), /* continue */ );
 
-		sleep(1);
+		sleep(30); /* Sleep 30 Seconds */
 	}
 	
 	free(buf);
 	return NULL;
 }
 
-/* Function called on receipt of MONITOR_SIGNAL */
-static void got_sig()
-{
-	char * buf = NULL;
-	size_t len;
-	TRACE_DEBUG(INFO, "[dbg_peers] Dumping config information");
-	TRACE_DEBUG(INFO, "%s", fd_conf_dump(&buf, &len, NULL));
-	TRACE_DEBUG(INFO, "[dbg_peers] Dumping extensions information");
-	TRACE_DEBUG(INFO, "%s", fd_ext_dump(&buf, &len, NULL));
-	TRACE_DEBUG(INFO, "[dbg_peers] Dumping dictionary information");
-	TRACE_DEBUG(INFO, "%s", fd_dict_dump(&buf, &len, NULL, fd_g_config->cnf_dict));
-	free(buf);
-}
-
 /* Entry point */
-static int monitor_main(char * conffile)
+static int monitor_peers_main(char * statefile)
 {
-	TRACE_ENTRY("%p", conffile);
+	TRACE_DEBUG(INFO, "[dbg_peers] Writing Diameter peers to %s", statefile);
 	
-	/* Catch signal SIGUSR1 */
-	CHECK_FCT( fd_event_trig_regcb(MONITOR_SIGNAL, "dbg_peers", got_sig));
-	
-	CHECK_POSIX( pthread_create( &thr, NULL, mn_thr, NULL ) );
+	CHECK_POSIX( pthread_create( &thr, NULL, mn_thr, statefile ) );
+
 	return 0;
 }
 
 /* Cleanup */
 void fd_ext_fini(void)
 {
-	TRACE_ENTRY();
 	CHECK_FCT_DO( fd_thr_term(&thr), /* continue */ );
+	TRACE_DEBUG(INFO, "[dbg_peers] Cleaning up files");
+	int ret;
+	ret = remove("/tmp/DiamPeers.txt");
+	if(ret == 0) {
+		printf("Diameter Peer state file deleted successfully");
+	} else {
+		printf("Error: unable to delete the file");
+	}	
 	return ;
 }
 
